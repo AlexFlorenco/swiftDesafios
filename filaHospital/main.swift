6f5e8739df43a15b1d1c72d2f85a4d3cc2c2e2f8
Alexandre Floren;o <aflorenco13@gmail.com>
2023-03-29 22:11:52 -0300

    test: Filas para ambiente de teste

--- filaHospital/main.swift
+++ filaHospital/main.swift
@@ -1,13 +1,14 @@
 import filaHospital
 
-var ultimaSenha = 0
-var fila1 = [[String]]()
-var fila2 = [[String]]()
-var fila3 = [[String]]()
+//var ultimaSenha = 0
+//var fila1 = [[String]]()
+//var fila2 = [[String]]()
+//var fila3 = [[String]]()
 
-//var fila1 = [["1", "Alex", "CRIANÇA"], ["4", "Nerice", "IDOSO"] ]
-//var fila2 = [["2", "Igor", "ADULTO"], ["5", "João", "ADULTO"], ["7", "Ana", "IDOSO"]]
-//var fila3 = [["3", "Jose", "IDOSO"], ["6", "Maria", "ADULTO"], ["8", "Jonas", "ADULTO"]]
+var ultimaSenha = 9
+var fila1 = [["1", "Igor", "ADULTO"], ["4", "João", "IDOSO"], ["7", "Ana", "CRIANÇA"]]
+var fila2 = [["2", "Alex", "ADULTO"]]
+var fila3 = [["3", "Jose", "IDOSO"], ["6", "Maria", "ADULTO"], ["9", "Jonas", "IDOSO"]]
 
 while true{
     print("\n1 - NOVO PACIENTE\n2 - PRÓXIMO ATENDIMENTO\n3 - DESISTÊNCIA\n4 - MOSTRAR FILAS\n> ", terminator: "")
