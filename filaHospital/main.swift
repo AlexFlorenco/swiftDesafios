e8a0bef4ada8df108561588a7de86b90f2922f32
Alexandre Floren;o <aflorenco13@gmail.com>
2023-03-31 12:49:47 -0300

    feat: Filas agora são arrays de objetos Paciente

--- filaHospital/main.swift
+++ filaHospital/main.swift
@@ -1,17 +1,17 @@
 import filaHospital
 
-//var ultimaSenha = 0
-//var fila1 = [[String]]()
-//var fila2 = [[String]]()
-//var fila3 = [[String]]()
+var ultimaSenha = 0
+var fila1 = [Paciente]()
+var fila2 = [Paciente]()
+var fila3 = [Paciente]()
 
-var ultimaSenha = 9
-var fila1 = [["1", "Igor", "ADULTO"], ["4", "João", "IDOSO"], ["7", "Ana", "CRIANÇA"]]
-var fila2 = [["2", "Alex", "ADULTO"]]
-var fila3 = [["3", "Jose", "IDOSO"], ["6", "Maria", "ADULTO"], ["9", "Jonas", "IDOSO"]]
+//var ultimaSenha = 9
+//var fila1 = [["1", "Igor", "ADULTO"], ["4", "João", "IDOSO"], ["7", "Ana", "CRIANÇA"]]
+//var fila2 = [["2", "Alex", "ADULTO"]]
+//var fila3 = [["3", "Jose", "IDOSO"], ["6", "Maria", "ADULTO"], ["9", "Jonas", "IDOSO"]]
 
 while true{
-    print("\n1 - NOVO PACIENTE\n2 - PRÓXIMO ATENDIMENTO\n3 - DESISTÊNCIA\n4 - MOSTRAR FILAS\n> ", terminator: "")
+    print("\n\n1 - NOVO PACIENTE\n2 - PRÓXIMO ATENDIMENTO\n3 - DESISTÊNCIA\n4 - MOSTRAR FILAS\n> ", terminator: "")
     if let operacao = readLine(){
         switch operacao {
         case "1":
@@ -24,7 +24,7 @@ while true{
             removerPacienteDaFila()
             
         case "4":
-            print("\nFila 1: \(fila1)\nFila 2: \(fila2)\nFila 3: \(fila3)")
+            mostrarFilas()
             
         default:
             print(">>> OPERAÇÃO INVÁLIDA!")
